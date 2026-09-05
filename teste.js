7c64f023adabfdec90470094f1971b03a101fe46
Pedro1570 <133928121+Pedro1570@users.noreply.github.com>
2026-09-05 14:13:54 -0400

    Remove bcrypt comparison code from teste.js

--- teste.js
+++ teste.js
@@ -1,4 +1,1 @@
-const bcrypt = require('bcrypt');
-const senha = '123456';
-const hash = '$2b$12$KkixMezgp49UUz0Mx9E0suWBFMnbw725EOAo/S4UXPJhhs3Iu8LKu';
-bcrypt.compare(senha, hash).then(console.log);
+
